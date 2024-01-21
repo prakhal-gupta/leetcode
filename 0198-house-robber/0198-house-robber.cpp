891fa40075a83acd9684fad6ca27d0c5004e5907
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-01-21 12:25:44 +0530

    Time: 0 ms (100.00%), Space: 9.4 MB (9.67%) - GitLeet

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int n = nums.size();
+        if(n==1 ){
+            return nums[0];
+        }
+        if(n==2){
+            return max(nums[0],nums[1]);
+        }
+        vector<int> dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
+    }
+};
