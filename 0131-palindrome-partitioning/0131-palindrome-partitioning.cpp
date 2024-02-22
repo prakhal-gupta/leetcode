210d2db23d3197f7f577825330d0d1f89f7850c6
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-22 15:05:54 +0530

    Time: 71 ms (92.04%), Space: 52.8 MB (82.36%) - GitLeet

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string &s, int start, int end){
+        while(start<end){
+            if(s[start++]!=s[end--])
+                return false;
+        }
+        return true;
+    }
+    
+    void partitionHelper(string& s, int start, vector<string>& partition, vector<vector<string>>& result) {
+        if (start == s.length()) {
+            result.push_back(partition);
+            return;
+        }
+
+        for (int end = start; end < s.length(); ++end) {
+            if (isPalindrome(s, start, end)) {
+                partition.push_back(s.substr(start, end - start + 1));
+                partitionHelper(s, end + 1, partition, result);
+                partition.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> partition;
+        partitionHelper(s, 0, partition, result);
+        return result;
+    }
+};
