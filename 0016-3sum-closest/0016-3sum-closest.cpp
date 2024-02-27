2faa684b908b1529431a074eac46b908abbfe70e
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-27 11:10:13 +0530

    Time: 19 ms (61.09%), Space: 12.7 MB (52.36%) - GitLeet

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int ans = INT_MAX;
+        int sum =0;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size();i++){
+            int j = i+1, k=nums.size()-1;
+            int tar = target - nums[i];
+            while(j<k){
+                // if(nums[j]+nums[k]==tar)
+                //     return 0;
+                // else if( nums[j]+nums[k]<tar){
+                //     ans = min(ans,abs(target - (nums[i] + nums[j] + nums[k])));
+                //     j++;
+                // }
+                // else{
+                //     ans = min(ans,abs(target - (nums[i] + nums[j] + nums[k])));
+                //     k--;
+                // }
+                if(abs(target - (nums[i] + nums[j] + nums[k])) < ans){
+                    ans = abs(target - (nums[i] + nums[j] + nums[k]));
+                    sum = nums[i] + nums[j] + nums[k];
+                }
+                if(nums[j]+nums[k]>=tar)
+                    k--;
+                else
+                    j++;
+            }
+        }
+        return sum;
+    }
+};
