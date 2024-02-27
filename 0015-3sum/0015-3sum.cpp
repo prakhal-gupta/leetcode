962cba97d87f820e4d4dec82438e7df2215349c4
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-27 10:30:50 +0530

    Time: 61 ms (83.47%), Space: 27.3 MB (55.59%) - GitLeet

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> resultant;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
+            int j = i+1, k=nums.size()-1;
+            while(j<k){
+                int target = - nums[i];
+                if(nums[j]+nums[k]==target){
+                    if(resultant.empty() || (nums[i] != resultant.back()[0] || nums[j] != resultant.back()[1])) {
+                        resultant.push_back({nums[i], nums[j], nums[k]});
+                    }
+                    j++;
+                    k--;
+                }
+                else if( nums[j]+nums[k]<=target)
+                    j++;
+                else
+                    k--;
+            }
+        }
+        return resultant;
+    }
+};
