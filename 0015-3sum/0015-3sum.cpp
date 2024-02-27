7e3bec68f95310d1a9b331939603378a237221e9
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-27 10:43:15 +0530

    Time: 63 ms (80.56%), Space: 27.2 MB (58.95%) - GitLeet

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -10,11 +10,17 @@ public:
             while(j<k){
                 int target = - nums[i];
                 if(nums[j]+nums[k]==target){
-                    if(resultant.empty() || (nums[i] != resultant.back()[0] || nums[j] != resultant.back()[1])) {
-                        resultant.push_back({nums[i], nums[j], nums[k]});
-                    }
-                    j++;
-                    k--;
+                    // if(resultant.empty() || (nums[i] != resultant.back()[0] || nums[j] != resultant.back()[1])) {
+                    //     resultant.push_back({nums[i], nums[j], nums[k]});
+                    // }
+                    // j++;
+                    // k--;
+                    resultant.push_back({nums[i], nums[j], nums[k]});
+                    int l =nums[j], r=nums[k];
+                    while(j<k && nums[j]==l)
+                        j++;
+                    while(j<k && nums[k]==r)
+                        k--;
                 }
                 else if( nums[j]+nums[k]<=target)
                     j++;
