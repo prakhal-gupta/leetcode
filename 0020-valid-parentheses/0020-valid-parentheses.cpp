1f26a9cba02a005033ef04c9dbe9fd20a73ea730
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-28 14:06:47 +0530

    Time: 4 ms (12.63%), Space: 7.6 MB (22.60%) - GitLeet

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s){
+            if(c == '(' || c=='{' || c== '[')
+                st.push(c);
+            else{
+                if(st.empty() || (c==')' && st.top()!='(') || (c=='}' && st.top()!='{') || (c==']' && st.top()!='['))
+                    return false;
+                st.pop();
+            }
+        }
+        if(st.empty())
+            return true;
+        return false;
+    }
+};
