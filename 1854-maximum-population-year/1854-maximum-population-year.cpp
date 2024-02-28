71091f7aaa6ce974a438cc349a47970610adc4f9
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-28 11:45:58 +0530

    Time: 0 ms (100.00%), Space: 10 MB (56.37%) - GitLeet

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -9,15 +9,17 @@ public:
         for (int i = 1; i < prefixSum.size(); i++)
             prefixSum[i] = prefixSum[i - 1] + prefixSum[i];
         
-        int maxPopulation = 0;
-        int maxYear = 0;
-        for (int i = 0; i < prefixSum.size(); i++) {
-            if (prefixSum[i] > maxPopulation) {
-                maxPopulation = prefixSum[i];
-                maxYear = i;
-            }
-        }
+        // int maxPopulation = 0;
+        // int maxYear = 0;
+        // for (int i = 0; i < prefixSum.size(); i++) {
+        //     if (prefixSum[i] > maxPopulation) {
+        //         maxPopulation = prefixSum[i];
+        //         maxYear = i;
+        //     }
+        // }
+        auto maxYearIter = max_element(prefixSum.begin(), prefixSum.end());
+        int maxIndex = distance(prefixSum.begin(), maxYearIter);
         
-        return maxYear + 1950;
+        return maxIndex + 1950;
     }
 };
