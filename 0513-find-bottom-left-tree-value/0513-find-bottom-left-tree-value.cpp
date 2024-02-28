20a280cd5ee3934a8bab7801cc8b25ff7d29c16d
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-28 07:03:53 +0530

    Time: 6 ms (83.03%), Space: 19.8 MB (99.95%) - GitLeet

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        maxDepth = -1;
+        bottomLeftValue = 0;
+        dfs(root, 0);
+        return bottomLeftValue;
+    }
+
+private:
+    int maxDepth;
+    int bottomLeftValue;
+
+    void dfs(TreeNode* current, int depth) {
+        if (current == nullptr) {
+            return;
+        }
+        if (depth > maxDepth) {  // If true, we discovered a new level
+            maxDepth = depth;
+            bottomLeftValue = current->val;
+        }
+        dfs(current->left, depth + 1);
+        dfs(current->right, depth + 1);
+    }
+};
+
