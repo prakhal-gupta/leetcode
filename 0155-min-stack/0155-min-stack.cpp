c0eb8e957b0a92fddff64831fd06f52dde7f19ff
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-29 11:34:36 +0530

    Time: 17 ms (61.75%), Space: 20.9 MB (35.55%) - GitLeet

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -12,11 +12,11 @@ public:
     }
     
     void pop() {
-        if(!st.empty()){
-            if(st.top()==temp.top())
-                temp.pop();
-            st.pop();
-        }
+        if(st.empty())
+            return;
+        if(st.top()==temp.top())
+            temp.pop();
+        st.pop();
     }
     
     int top() {
