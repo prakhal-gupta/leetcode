3c1ea7e40dea9a31f8484ec69eb732feec56ac43
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-02-29 08:02:06 +0530

    Time: 88 ms (97.65%), Space: 79.5 MB (73.62%) - GitLeet

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> s;
+        int maxArea = 0;
+        int i = 0;
+        while (i < heights.size()) {
+            if (s.empty() || heights[i] >= heights[s.top()]) {
+                s.push(i);
+                i++;
+            } else {
+                int tp = s.top();
+                s.pop();
+                int width = s.empty() ? i : i - s.top() - 1;
+                maxArea = max(maxArea, heights[tp] * width);
+            }
+        }
+        
+        while (!s.empty()) {
+            int tp = s.top();
+            s.pop();
+            int width = s.empty() ? i : i - s.top() - 1;
+            maxArea = max(maxArea, heights[tp] * width);
+        }
+        
+        return maxArea;
+    }
+};
