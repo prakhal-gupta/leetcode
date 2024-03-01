98bb2efa7188f0c0ef6ecfaf664e4d895e1c1af4
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-03-01 12:05:44 +0530

    Update 0206-reverse-linked-list.cpp

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,6 +11,13 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        //  if (head == nullptr || head->next == nullptr) {
+        //     return head;
+        // }
+        // ListNode* reversedList = reverseList(head->next);
+        // head->next->next = head;
+        // head->next = nullptr;
+        // return reversedList;
         ListNode *current = head;
         ListNode *previous = nullptr;
         while(current!=nullptr){
