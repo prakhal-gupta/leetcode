c19d20bfa1e7f24a4fa62a11c08803eaf2ec9dec
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-03-01 14:46:12 +0530

    Time: 7 ms (86.43%), Space: 10.5 MB (49.87%) - GitLeet

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *s=head, *f=head;
+        
+        while(f!=nullptr && f->next!=nullptr){
+            s=s->next;
+            f=f->next->next;
+            if(s==f)
+                return true;
+        }
+        return false;
+    }
+};
