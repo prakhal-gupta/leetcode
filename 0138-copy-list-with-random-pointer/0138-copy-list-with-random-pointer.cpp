b499bce43cfaede7f1df9fb67ed67f593ef93fcf
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-03-14 16:09:10 +0530

    Time: 3 ms (91.74%), Space: 12.8 MB (80.42%) - GitLeet

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,18 +19,45 @@ public:
     Node* copyRandomList(Node* head) {
         if(!head)
             return nullptr;
-        unordered_map<Node*, Node*> cpy;
+        // unordered_map<Node*, Node*> cpy;
+        // Node* ptr = head;
+        // while(ptr) {
+        //     cpy[ptr] = new Node(ptr->val);
+        //     ptr = ptr->next;
+        // }
+        // ptr = head;
+        // while(ptr) {
+        //     cpy[ptr]->next = cpy[ptr->next];
+        //     cpy[ptr]->random = cpy[ptr->random];
+        //     ptr = ptr->next;
+        // }
+        // return cpy[head];
+        
         Node* ptr = head;
         while (ptr) {
-            cpy[ptr] = new Node(ptr->val);
-            ptr = ptr->next;
+            Node* temp = ptr->next;
+            ptr->next = new Node(ptr->val);
+            ptr->next->next = temp;
+            ptr = temp;
         }
         ptr = head;
         while (ptr) {
-            cpy[ptr]->next = cpy[ptr->next];
-            cpy[ptr]->random = cpy[ptr->random];
-            ptr = ptr->next;
+            if (ptr->random)
+                ptr->next->random = ptr->random->next;
+            ptr = ptr->next->next;
         }
-        return cpy[head];
+        ptr = head;
+        Node* newHead = ptr->next;
+        Node* newPtr = newHead;
+        while (ptr) {
+            ptr->next = newPtr->next;
+            ptr = ptr->next;
+            if (ptr) {
+                newPtr->next = ptr->next;
+                newPtr = newPtr->next;
+            }
+        }
+
+        return newHead;
     }
 };
