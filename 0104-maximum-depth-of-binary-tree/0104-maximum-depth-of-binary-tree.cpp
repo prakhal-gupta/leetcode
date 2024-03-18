31c23bd2a9251e39f95768286816ebc2f6b8020b
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-03-18 15:23:18 +0530

    Time: 9 ms (19.57%), Space: 17.4 MB (47.19%) - GitLeet

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -14,8 +14,6 @@ public:
     int maxDepth(TreeNode* root) {
         if(!root)
             return 0;
-        if(!root->left && !root->right)
-            return 1;
         return 1+ max(maxDepth(root->left),maxDepth(root->right));
     }
 };
