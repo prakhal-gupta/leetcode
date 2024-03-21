ab2cb0592c0dc2064d1b6869672e9f16b7f723e4
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-03-21 14:49:04 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (89.64%) - GitLeet

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,18 +11,37 @@
  */
 class Solution {
 public:
-    void postorder(TreeNode*&root,vector<int>&ans){
-        if(root){
-            postorder(root->left,ans);
-            postorder(root->right,ans);
-            ans.push_back(root->val);
-        }
-    }
+    // void postorder(TreeNode*&root,vector<int>&ans){
+    //     if(root){
+    //         postorder(root->left,ans);
+    //         postorder(root->right,ans);
+    //         ans.push_back(root->val);
+    //     }
+    // }
+    // vector<int> postorderTraversal(TreeNode* root) {
+    //     if(!root)
+    //         return {};
+    //     vector<int> ans;
+    //     postorder(root,ans);
+    //     return ans;
+    // }
+    
     vector<int> postorderTraversal(TreeNode* root) {
         if(!root)
-            return {};
+                return {};
         vector<int> ans;
-        postorder(root,ans);
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+            ans.insert(ans.begin(), curr->val);
+            if(curr->left)
+                st.push(curr->left);
+            if(curr->right)
+                st.push(curr->right);
+        }
         return ans;
     }
 };
+    
