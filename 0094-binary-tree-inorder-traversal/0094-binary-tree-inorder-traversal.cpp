0f31181c1e5ce1fbfe622112109ca3d5f8fcda70
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-03-21 14:41:59 +0530

    Time: 4 ms (31.70%), Space: 10 MB (16.76%) - GitLeet

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,18 +11,41 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode*&root,vector<int>&ans){
-        if(root){
-            inorder(root->left,ans);
-            ans.push_back(root->val);
-            inorder(root->right,ans);
-        }
-    }
+    // void inorder(TreeNode*&root,vector<int>&ans){
+    //     if(root){
+    //         inorder(root->left,ans);
+    //         ans.push_back(root->val);
+    //         inorder(root->right,ans);
+    //     }
+    // }
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     if(!root)
+    //         return {};
+    //     vector<int> ans;
+    //     inorder(root,ans);
+    //     return ans;
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         if(!root)
-            return {};
+                return {};
         vector<int> ans;
-        inorder(root,ans);
+        stack<pair<TreeNode*, bool>> st;
+        st.push({root, false});
+        while(!st.empty()){
+            auto curr = st.top();
+            st.pop();
+            if (curr.first) {
+                if (!curr.second) {
+                    if(curr.first->right)
+                        st.push({curr.first->right, false});
+                    st.push({curr.first, true});
+                    if(curr.first->left)
+                        st.push({curr.first->left, false});
+                } else {
+                    ans.push_back(curr.first->val);
+                }
+            }
+        }
         return ans;
     }
 };
