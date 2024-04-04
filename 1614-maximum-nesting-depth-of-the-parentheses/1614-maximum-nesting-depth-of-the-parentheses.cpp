18769298ab49da43910d419af1ab70e6171bf335
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-04-04 05:35:48 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (22.85%) - GitLeet

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans = 0;
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(')
+                st.push(c);
+            else if (c == ')')
+                st.pop();
+            ans = max(ans, (int)st.size());
+        }
+        return ans;
+    }
+};
