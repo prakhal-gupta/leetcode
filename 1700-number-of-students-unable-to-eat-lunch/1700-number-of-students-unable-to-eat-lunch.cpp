da03c0e1400cc8f3273efb3cac4caf19ba4c757f
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-04-08 08:01:02 +0530

    Time: 6 ms (6.19%), Space: 10.8 MB (79.46%) - GitLeet

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int n = students.size();
-    std::vector<bool> eaten(n, false); // Keeps track of whether a student has eaten or not
-    int sandwichIndex = 0; // Index for iterating through sandwiches
+    std::vector<bool> eaten(n, false);
+    int sandwichIndex = 0;
 
     while (sandwichIndex < n) {
-        bool satisfied = false; // Flag to indicate if any student can eat the current sandwich
+        bool satisfied = false;
         for (int i = 0; i < n; ++i) {
             if (!eaten[i] && students[i] == sandwiches[sandwichIndex]) {
-                eaten[i] = true; // Mark the student as eaten
-                satisfied = true; // Mark as satisfied
-                break; // Move to the next sandwich
+                eaten[i] = true;
+                satisfied = true;
+                break;
             }
         }
         if (!satisfied) {
-            break; // No student can eat the current sandwich, so break the loop
+            break;
         }
         sandwichIndex++;
     }
