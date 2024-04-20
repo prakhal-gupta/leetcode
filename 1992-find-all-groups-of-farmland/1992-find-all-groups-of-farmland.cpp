eb9af86b5405f6bc61d5a033063a64367c32c31b
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-04-20 14:44:39 +0530

    Time: 488 ms (16.73%), Space: 97.5 MB (30.55%) - GitLeet

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -3,15 +3,16 @@ public:
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         vector<vector<bool>> visited(land.size(), vector<bool>(land[0].size(), false));
         vector<vector<int>> ans;
-        for (int row = 0; row < land.size(); row++) {
-            for (int col = 0; col < land[0].size(); col++) {
-                if (land[row][col] && !visited[row][col]) {
-                    int bottomRow = row, rightCol = col;
-                    DFS(land, visited, row, col, bottomRow, rightCol);
-                    ans.push_back({row, col, bottomRow, rightCol});
+        for (int row1 = 0; row1 < land.size(); row1++) {
+            for (int col1 = 0; col1 < land[0].size(); col1++) {
+                if (land[row1][col1] && !visited[row1][col1]) {
+                    int row2 = 0, col2 = 0;
+                    DFS(land, visited, row1, col1, row2, col2);
+                    ans.push_back({row1, col1, row2, col2});
                 }
             }
         }
+        
         return ans;
     }
 
@@ -21,15 +22,15 @@ private:
         return x >= 0 && x < N && y >= 0 && y < M;
     }
     
-    void DFS(vector<vector<int>>& land, vector<vector<bool>>& visited, int x, int y, int& bottomRow, int& rightCol) {
+    void DFS(vector<vector<int>>& land, vector<vector<bool>>& visited, int x, int y, int& row2,
+        int& col2) {
         visited[x][y] = true;
-        bottomRow = max(bottomRow, x);
-        rightCol = max(rightCol, y);
+        row2 = max(row2, x); col2 = max(col2, y);
         for (auto dir : dirs) {
             int newX = x + dir[0], newY = y + dir[1];
             if (isWithinFarm(newX, newY, land.size(), land[0].size()) && !visited[newX][newY]
                     && land[newX][newY]) {
-                DFS(land, visited, newX, newY, bottomRow, rightCol);
+                DFS(land, visited, newX, newY, row2, col2);
             }
         }
     }    
