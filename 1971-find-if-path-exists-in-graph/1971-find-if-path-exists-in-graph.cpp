d33fc18440892a103e0b815025c6fb9e19904601
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-04-21 07:10:42 +0530

    Time: 500 ms (50.52%), Space: 181.6 MB (61.68%) - GitLeet

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int,vector<int>>umap;
-        for(auto x:edges){
-            vector<int>temp=x;
-            int u=temp[0];
-            int v=temp[1];
-            umap[u].push_back(v);
-            umap[v].push_back(u); 
+        // Using vector instead of unordered_map for faster access
+        vector<vector<int>> graph(n);
+        for(auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
-        vector<bool>visited(n,false);
-        queue<int>q;
+
+        vector<bool> visited(n, false);
+        queue<int> q;
         q.push(source);
-        visited[source]=true;
-        while(!q.empty()){
-            int v=q.front();
+        visited[source] = true;
+        
+        while(!q.empty()) {
+            int v = q.front();
             q.pop();
-                 vector<int>temp=umap[v];
-                for(int i=0;i<temp.size();i++){
-                    int vertex =temp[i];
-                    if(visited[vertex]==false){
-                        q.push(vertex);
-                        visited[vertex]=true;
-                    }
+
+            if(v == destination) // Destination reached
+                return true;
+
+            for(int neighbor : graph[v]) {
+                if(!visited[neighbor]) {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
                 }
+            }
         }
-        return visited[destination];  
+        
+        return false;
     }
 };
