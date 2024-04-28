2c54e44837f951d8326e6e6ce12939902d15f7ae
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-04-28 14:24:24 +0530

    Time: 434 ms (14.07%), Space: 111.2 MB (21.48%) - GitLeet

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void dfs(int root, vector<vector<int>>& graph, vector<int>& count, vector<int>& ans, vector<bool>& visited) {
+    void dfs(int root, vector<vector<int>>& graph, vector<int>& count, vector<int>& ans, vector<bool>& visited, int parent) {
         visited[root] = true;
         for (auto child : graph[root]) {
             if (!visited[child]) {
-                dfs(child, graph, count, ans, visited);
+                dfs(child, graph, count, ans, visited, root);
                 count[root] += count[child];
                 ans[root] += ans[child] + count[child];
             }
         }
     }
 
-    void dfs2(int root, vector<vector<int>>& graph, vector<int>& count, vector<int>& ans, vector<bool>& visited) {
+    void dfs2(int root, vector<vector<int>>& graph, vector<int>& count, vector<int>& ans, vector<bool>& visited, int parent) {
         visited[root] = true;
         for (auto child : graph[root]) {
             if (!visited[child]) {
                 ans[child] = ans[root] - count[child] + count.size() - count[child];
-                dfs2(child, graph, count, ans, visited);
+                dfs2(child, graph, count, ans, visited, root);
             }
         }
     }
@@ -29,9 +29,9 @@ public:
         }
         vector<int> count(n, 1), ans(n, 0);
         vector<bool> visited(n, false);
-        dfs(0, graph, count, ans, visited);
+        dfs(0, graph, count, ans, visited, -1);
         visited.assign(n, false);
-        dfs2(0, graph, count, ans, visited);
+        dfs2(0, graph, count, ans, visited, -1);
         return ans;
     }
 };
