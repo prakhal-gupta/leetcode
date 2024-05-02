1b7091ae7260902a5eacc4b9c01cebd343ff0a40
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-05-02 07:33:44 +0530

    Time: 29 ms (17.89%), Space: 26.6 MB (21.19%) - GitLeet

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int findMaxK(vector<int>& nums) {
+    int findMaxK(std::vector<int>& nums) {
+        std::unordered_set<int> numSet(nums.begin(), nums.end());
         int ans = -1;
-        
-        for (int i : nums) {
-            for (int j : nums) {
-                // If there exists a number j such that i is the negative of j
-                if (i == -j) {
-                    // Update the answer to the maximum of current ans and absolute value of i
-                    ans = max(ans, abs(i));
-                }
-            }
+        for (int i : numSet) {
+            if (numSet.find(-i) != numSet.end())
+                ans = std::max(ans, std::abs(i));
         }
-
         return ans;
     }
 };
