3338daab7f629675a4f6690e61999041611ff704
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-05-05 23:00:03 +0530

    Time: 4 ms (80.20%), Space: 11.3 MB (84.10%) - GitLeet

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        // Overwrite data of next node on current node.
+        node->val = node->next->val;
+        // Make current node point to next of next node.
+        node->next = node->next->next;
+    }
+};
