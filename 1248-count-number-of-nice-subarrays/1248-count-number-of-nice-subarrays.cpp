d485ad16e89deab977f17a09df76ff2d84344775
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-06-22 08:05:51 +0530

    Time: 164 ms (14.73%), Space: 94 MB (12.05%) - GitLeet

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int currSum = 0, subarrays = 0;
+        unordered_map<int, int> prefixSum;
+        prefixSum[currSum] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            currSum += nums[i] % 2;
+            if (prefixSum.find(currSum - k) != prefixSum.end())
+                subarrays = subarrays + prefixSum[currSum - k];
+            prefixSum[currSum]++;
+        }
+        return subarrays;
+    }
+};
