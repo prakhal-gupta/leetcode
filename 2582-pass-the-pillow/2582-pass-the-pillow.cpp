f7a096fc3a352cb6afbc4b0759f72103767b8c8c
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-07-06 10:42:54 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (30.58%) - GitLeet

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int currentPillowPosition = 1;
+        int currentTime = 0;
+        int direction = 1;
+        while (currentTime < time) {
+            if (0 < currentPillowPosition + direction &&
+                currentPillowPosition + direction <= n) {
+                currentPillowPosition += direction;
+                currentTime++;
+            } else
+                direction *= -1;
+        }
+        return currentPillowPosition;
+    }
+};
