a97860acb25f94f1ce6b05bf90b2e9c08de0460d
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-07-12 08:53:01 +0530

    Time: 108 ms (35.77%), Space: 30.4 MB (10.16%) - GitLeet

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int totalScore = 0;
+        string highPriorityPair = x > y ? "ab" : "ba";
+        string lowPriorityPair = highPriorityPair == "ab" ? "ba" : "ab";
+        string stringAfterFirstPass = removeSubstring(s, highPriorityPair);
+        int removedPairsCount = (s.length() - stringAfterFirstPass.length()) / 2;
+        totalScore += removedPairsCount * max(x, y);
+        string stringAfterSecondPass = removeSubstring(stringAfterFirstPass, lowPriorityPair);
+        removedPairsCount = (stringAfterFirstPass.length() - stringAfterSecondPass.length()) /2;
+        totalScore += removedPairsCount * min(x, y);
+        return totalScore;
+    }
+
+private:
+    string removeSubstring(const string& input, const string& targetPair) {
+        stack<char> charStack;
+        for (char currentChar : input) {
+            if (currentChar == targetPair[1] && !charStack.empty() &&
+                charStack.top() == targetPair[0]) {
+                charStack.pop();
+            } else
+                charStack.push(currentChar);
+        }
+        string remainingChars;
+        while (!charStack.empty()) {
+            remainingChars += charStack.top();
+            charStack.pop();
+        }
+        reverse(remainingChars.begin(), remainingChars.end());
+        return remainingChars;
+    }
+};
