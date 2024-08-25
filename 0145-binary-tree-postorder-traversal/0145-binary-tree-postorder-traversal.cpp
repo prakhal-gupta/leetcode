7548654cacfd21340f682e4c072d5421a10d482e
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-08-25 09:29:52 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (44.35%) - GitLeet

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,47 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    // void postorder(TreeNode*&root,vector<int>&ans){
-    //     if(root){
-    //         postorder(root->left,ans);
-    //         postorder(root->right,ans);
-    //         ans.push_back(root->val);
-    //     }
-    // }
-    // vector<int> postorderTraversal(TreeNode* root) {
-    //     if(!root)
-    //         return {};
-    //     vector<int> ans;
-    //     postorder(root,ans);
-    //     return ans;
-    // }
-    
+    void postorderTraversalHelper(TreeNode* currentNode, vector<int>& result) {
+        if (!currentNode)
+            return;
+        postorderTraversalHelper(currentNode->left, result);
+        postorderTraversalHelper(currentNode->right, result);
+        result.push_back(currentNode->val);
+    }
+
     vector<int> postorderTraversal(TreeNode* root) {
-        if(!root)
-                return {};
-        vector<int> ans;
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            TreeNode* curr = st.top();
-            st.pop();
-            ans.insert(ans.begin(), curr->val);
-            if(curr->left)
-                st.push(curr->left);
-            if(curr->right)
-                st.push(curr->right);
-        }
-        return ans;
+        vector<int> result;
+        postorderTraversalHelper(root, result);
+        return result;
     }
 };
-    
