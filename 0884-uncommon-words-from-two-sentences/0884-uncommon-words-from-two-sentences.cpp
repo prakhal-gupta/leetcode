466ffa5fd141affd5792a4f2d823baf346a6afb9
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-09-17 06:36:27 +0530

    Time: 4 ms (40.63%), Space: 9 MB (28.22%) - GitLeet

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2){
+        vector<string>res;
+        string s=s1+" "+s2,token;
+        unordered_map<string,int>mp;
+        stringstream part(s);
+        while(part >> token) 
+            mp[token]++;
+        for(auto &it:mp)if(mp[it.first]==1) 
+            res.push_back(it.first);
+        return res;
+    }
+};
