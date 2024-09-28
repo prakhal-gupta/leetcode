8b55dc49f18af5d3fe1afdd17c6da2ca0357d272
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-09-28 19:34:19 +0530

    Time: 12 ms (38.24%), Space: 22 MB (30.77%) - GitLeet

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL)  return true;
+		if (Height(root) == -1)  return false;
+		return true;
+	}
+
+	int Height(TreeNode* root) {
+		if (root == NULL)  return 0;
+		int leftHeight = Height(root->left);
+		int rightHight = Height(root->right);
+		if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)  return -1;
+		return max(leftHeight, rightHight) + 1;
+    }
+};
