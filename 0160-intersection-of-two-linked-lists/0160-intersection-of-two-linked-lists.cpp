dbe74fb554b4167330e09c707352671a6effa13b
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2024-09-28 19:35:26 +0530

    Time: 565 ms (5.56%), Space: 17.4 MB (24.76%) - GitLeet

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		ListNode *temp;
+		while(headA != NULL){
+			temp = headB;
+			while(temp != NULL){
+				if(headA == temp)
+					return headA;
+				temp = temp -> next;
+			}
+			headA = headA -> next;
+		}
+		return NULL;
+	}
+};
