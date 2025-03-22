cc0637d638b495d888f651907a758297e86c4267
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2025-03-22 08:42:13 +0530

    Time: 151 ms (8.24%) | Memory: 143 MB (18.50%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        unordered_map<string, int> componentFreq;
+        for (int vertex = 0; vertex < n; vertex++)
+            graph[vertex].push_back(vertex);
+        for (const auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        for (int vertex = 0; vertex < n; vertex++) {
+            vector<int> neighbors = graph[vertex];
+            sort(neighbors.begin(), neighbors.end());
+            string key;
+            for (int num : neighbors)
+                key += to_string(num) + ",";
+            componentFreq[key]++;
+        }
+        int completeCount = 0;
+        for (const auto& entry : componentFreq) {
+            int size = count(entry.first.begin(), entry.first.end(), ',');
+            if (size == entry.second) completeCount++;
+        }
+        return completeCount;
+    }
+};
