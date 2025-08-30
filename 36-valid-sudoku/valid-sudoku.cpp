699eb39ea53a6282fc97b02c1d72d361411ff184
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2025-08-30 20:42:35 +0530

    Time: 2 ms (66.40%) | Memory: 21.9 MB (80.79%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9] = {0}, columns[9] = {0}, boxes[9] = {0};
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.') continue;
-                int num = board[i][j] - '1';
-                int mask = 1 << num;
-                int boxIndex = (i / 3) * 3 + j / 3;
-                if (rows[i] & mask || columns[j] & mask || boxes[boxIndex] & mask)
-                    return false;
-                rows[i] |= mask;
-                columns[j] |= mask;
-                boxes[boxIndex] |= mask;
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) return false;
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+                }
             }
         }
         return true;
