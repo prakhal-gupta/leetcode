bbbacb47d094e47508eefe4171abec347e2b5bfb
Prakhal Gupta <103833711+prakhal-gupta@users.noreply.github.com>
2026-03-14 16:27:32 +0530

    Time: 23 ms (26.85%) | Memory: 25.6 MB (22.02%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -2,26 +2,34 @@ class Solution {
 public:
     string getHappyString(int n, int k) {
         string currentString = "";
-        string result = "";
-        int indexInSortedList = 0;
-        generateHappyStrings(n, k, currentString, indexInSortedList, result);
-        return result;
+        vector<string> happyStrings;
+        // Generate all happy strings of length n
+        generateHappyStrings(n, currentString, happyStrings);
+
+        // Check if there are at least k happy strings
+        if (happyStrings.size() < k) return "";
+
+        return happyStrings[k - 1];
     }
 
 private:
-    void generateHappyStrings(int n, int k, string &currentString, int &indexInSortedList, string &result) {
+    void generateHappyStrings(int n, string currentString,
+                              vector<string> &happyStrings) {
+        // If the current string has reached the desired length, add it to the
+        // list
         if (currentString.size() == n) {
-            indexInSortedList++;
-            if (indexInSortedList == k) result = currentString;
+            happyStrings.push_back(currentString);
             return;
         }
+
+        // Try adding each character ('a', 'b', 'c') to the current string
         for (char currentChar = 'a'; currentChar <= 'c'; currentChar++) {
+            // Skip if the current character is the same as the last character
             if (currentString.size() > 0 && currentString.back() == currentChar)
                 continue;
-            currentString += currentChar;
-            generateHappyStrings(n, k, currentString, indexInSortedList, result);
-            if (result != "") return;
-            currentString.pop_back();
+
+            // Recursively generate the next character
+            generateHappyStrings(n, currentString + currentChar, happyStrings);
         }
     }
 };
